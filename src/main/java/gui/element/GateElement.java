76f6fd45dbf1bd301a26f63b0c0522cf4765d99b
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-05-24 14:08:42 +0200

    Not using those classes anymore, highly duplicated code

--- src/main/java/gui/element/GateElement.java
+++ src/main/java/gui/element/GateElement.java
@@ -1,59 +1,59 @@
 package gui.element;
-
-import gui.Controller;
-import gui.element.shape.GateShape;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import model.elements.BinarySensor;
-
-public class GateElement implements IGUI {
-
-	private GateShape shape;
-	private BinarySensor openSensor, closeSensor;
-	private BooleanProperty stateOpenSensor = new SimpleBooleanProperty(false);
-	private BooleanProperty stateCloseSensor = new SimpleBooleanProperty(false);
-
-	public GateElement(GateShape shape, Controller controller, BinarySensor openSensor, BinarySensor closeSensor) {
-		this.shape = shape;
-		this.openSensor = openSensor;
-		this.closeSensor = closeSensor;
-		
-		stateOpenSensor.addListener(new ChangeListener<Boolean>() {
-			@Override
-			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-				if (newValue) {
-					shape.gateIsOpen();
-				} else {
-					shape.gateIsMoving();
-				}
-			}
-		});
-
-		stateCloseSensor.addListener(new ChangeListener<Boolean>() {
-			@Override
-			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-				if (newValue) {
-					shape.gateIsClosed();
-				} else {
-					shape.gateIsMoving();
-				}
-			}
-		});
-	}
-
-	@Override
-	public void update() {
-		if (openSensor != null)
-			stateOpenSensor.set(openSensor.getState());
-		if (closeSensor != null)
-			stateCloseSensor.set(closeSensor.getState());
-	}
-
-	@Override
-	public void reset() {
-		shape.gateIsMoving();
-	}
-
-}
+//
+//import gui.Controller;
+//import gui.element.shape.GateDoorShape;
+//import javafx.beans.property.BooleanProperty;
+//import javafx.beans.property.SimpleBooleanProperty;
+//import javafx.beans.value.ChangeListener;
+//import javafx.beans.value.ObservableValue;
+//import model.elements.BinarySensor;
+//
+public class GateElement { }
+//
+//	private GateDoorShape shape;
+//	private BinarySensor openSensor, closeSensor;
+//	private BooleanProperty stateOpenSensor = new SimpleBooleanProperty(false);
+//	private BooleanProperty stateCloseSensor = new SimpleBooleanProperty(false);
+//
+//	public GateElement(GateDoorShape shape, Controller controller, BinarySensor openSensor, BinarySensor closeSensor) {
+//		this.shape = shape;
+//		this.openSensor = openSensor;
+//		this.closeSensor = closeSensor;
+//		
+//		stateOpenSensor.addListener(new ChangeListener<Boolean>() {
+//			@Override
+//			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+//				if (newValue) {
+//					shape.gateIsOpen();
+//				} else {
+//					shape.gateIsMoving();
+//				}
+//			}
+//		});
+//
+//		stateCloseSensor.addListener(new ChangeListener<Boolean>() {
+//			@Override
+//			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+//				if (newValue) {
+//					shape.gateIsClosed();
+//				} else {
+//					shape.gateIsMoving();
+//				}
+//			}
+//		});
+//	}
+//
+//	@Override
+//	public void update() {
+//		if (openSensor != null)
+//			stateOpenSensor.set(openSensor.getState());
+//		if (closeSensor != null)
+//			stateCloseSensor.set(closeSensor.getState());
+//	}
+//
+//	@Override
+//	public void reset() {
+//		shape.gateIsMoving();
+//	}
+//
+//}
--- src/main/java/gui/element/SensorElement.java
+++ src/main/java/gui/element/SensorElement.java
@@ -1,44 +1,46 @@
 package gui.element;
-
-import gui.Controller;
-import gui.element.shape.SensorShape;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import model.elements.BinarySensor;
-
-public class SensorElement implements IGUI {
-
-	private SensorShape shape;
-	private BinarySensor logic;
-	private BooleanProperty state = new SimpleBooleanProperty(false);
-
-	public SensorElement(SensorShape shape, BinarySensor logic, Controller controller) {
-		this.shape = shape;
-		this.logic = logic;
-		
-		state.addListener(new ChangeListener<Boolean>() {
-			@Override
-			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-				if (newValue) {
-					shape.activateSensor();
-				} else {
-					shape.deactivateSensor();
-				}
-			}
-		});
-	}
-
-	@Override
-	public void update() {
-		if (logic != null)
-			state.set(logic.getState());
-	}
-
-	@Override
-	public void reset() {
-		shape.deactivateSensor();
-	}
+//
+//import gui.Controller;
+//import gui.element.shape.SensorShape;
+//import javafx.beans.property.BooleanProperty;
+//import javafx.beans.property.SimpleBooleanProperty;
+//import javafx.beans.value.ChangeListener;
+//import javafx.beans.value.ObservableValue;
+//import model.elements.BinarySensor;
+//
+public class SensorElement {
 
 }
+//
+//	private SensorShape shape;
+//	private BinarySensor logic;
+//	private BooleanProperty state = new SimpleBooleanProperty(false);
+//
+//	public SensorElement(SensorShape shape, BinarySensor logic, Controller controller) {
+//		this.shape = shape;
+//		this.logic = logic;
+//		
+//		state.addListener(new ChangeListener<Boolean>() {
+//			@Override
+//			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+//				if (newValue) {
+//					shape.activateSensor();
+//				} else {
+//					shape.deactivateSensor();
+//				}
+//			}
+//		});
+//	}
+//
+//	@Override
+//	public void update() {
+//		if (logic != null)
+//			state.set(logic.getState());
+//	}
+//
+//	@Override
+//	public void reset() {
+//		shape.deactivateSensor();
+//	}
+//
+//}
