8237e8b90cbbf44af25f20f99b25bcbf0904c002
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-05-25 15:10:44 +0200

    Moved to shape package

---
+++ src/main/java/gui/element/shape/ShapeHelper.java
@@ -0,0 +1,18 @@
+package gui.element.shape;
+
+import javafx.scene.paint.Color;
+
+public class ShapeHelper {
+
+	public static Color INACTIVE = Color.web("#dcdcdc");
+	public static Color ACTIVE = Color.GREEN;
+	public static Color SENSOR_FRONT = Color.web("#ff9021");
+	
+	public static Color GATE_OPEN = Color.GREEN;
+	public static Color GATE_CLOSED = Color.RED;
+	
+	public static Color BLOCK_COLOR;
+	public static Color RED_BLOCK = Color.RED;
+	public static Color BLUE_BLOCK = Color.BLUE;
+
+}
