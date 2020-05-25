31ea7a4671278f2b86878fcece09ed72f75eead5
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-05-25 15:13:19 +0200

    Added method to get all simple sensors not connected to an active simulation element

--- src/main/java/model/elements/SensorDefinition.java
+++ src/main/java/model/elements/SensorDefinition.java
@@ -1,5 +1,7 @@
 package model.elements;
 
+import java.util.EnumSet;
+
 import gui.element.Direction;
 
 /**
@@ -8,10 +10,10 @@ import gui.element.Direction;
 public enum SensorDefinition {
 //	B1_S01("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S01", "B1_S01", "Reedkontakt Registerlager", 0, 0, Direction.North),
 	B1_S02("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S02", "B1_S02", "Lichtschranke Registerlager", 1010, 310, Direction.North),
-	B1_S03("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S03", "B1_S02", "Initiator Förderband 1.1", 780, 230, Direction.South),
-//	B1_S04("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S04", "B1_S04", "Reedkontakt Farberkennung links", 0, 0, Direction.North),
-//	B1_S05("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S05", "B1_S05", "Reedkontakt Farberkennung rechts", 0, 0, Direction.North),
-	B1_S06("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S06", "B1_S06", "Taster Registerlager", 1110, 230, Direction.North),
+	B1_S03("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S03", "B1_S03", "Initiator Förderband 1.1", 780, 230, Direction.South),
+	B1_S04("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S04", "B1_S04", "Reedkontakt Farberkennung links", 900, 310 , Direction.North),
+	B1_S05("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S05", "B1_S05", "Reedkontakt Farberkennung rechts", 900, 230, Direction.South),
+	B1_S06("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S06", "B1_S06", "Taster Registerlager", 1010, 230, Direction.North),
 	
 	B1_S07("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S07", "B1_S07", "Initiator Förderband 1.2", 610, 310, Direction.North),
 	B1_S08("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S08", "B1_S08", "Initiator Förderband 1.3 vorn", 500, 310, Direction.North),
@@ -28,7 +30,7 @@ public enum SensorDefinition {
 //	B1_S18("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S18", "B1_S18", "Tor öffnen Einfahrt (Zugseil)", 0, 0, Direction.North),
 //	B1_S19("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S19", "B1_S19", "Tor öffnen Ausfahrt (Zugseil)", 0, 0, Direction.North),
 	
-//	B1_S20("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S20", "B1_S20", "Initiator Drehtisch", 0, 0, Direction.North),
+	B1_S20("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S20", "B1_S20", "Initiator Drehtisch", 0, 0, Direction.North),
 	B1_S21("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S21", "B1_S21", "Drehtisch Anschlag vertikal", 310, 190, Direction.East),
 	B1_S22("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S22", "B1_S22", "Drehtisch Anschlag horizontal", 190, 310, Direction.North),
 	B1_S23("ns=4;s=|var|CODESYS Control Win V3 x64.Application.Globale_Variablen.B1_S23", "B1_S23", "Initiator Förderband ganz links", 100, 230, Direction.South),
@@ -63,15 +65,19 @@ public enum SensorDefinition {
 		return this.comment;
 	}
 	
-	public int getX() {
-		return this.x;
+	public double getX() {
+		return (double) this.x;
 	}
 	
-	public int getY() {
-		return this.y;
+	public double getY() {
+		return (double) this.y;
 	}
 	
 	public Direction getDirection() {
 		return this.direction;
+	}
+	
+	public static EnumSet<SensorDefinition> getConveyorSensors() {
+		return EnumSet.of(B1_S02, B1_S03, B1_S04, B1_S05, B1_S07, B1_S08, B1_S09, B1_S16, B1_S17, B1_S23, B1_S24);
 	}
 }
