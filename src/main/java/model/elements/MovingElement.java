3d38c7b6d494207d017900c2b0423d1d87f0b709
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-05-25 08:54:29 +0200

    Renamed / moved

--- src/main/java/model/elements/MovingElement.java
+++ src/main/java/model/elements/MovingElement.java
@@ -2,24 +2,24 @@ package model.elements;
 
 import model.simulation.FtPlantSimulation;
 
-public abstract class LinearMovementElement extends SimulationUpdateable {
+public abstract class MovingElement extends SimulationUpdateable {
 
 	private final int actuatorVelocity = 20; 	// in mm per sec
-	protected int length;							// in mm
+	protected int distance;							// in mm
 	protected int stepSize = 1;
 	
-	public LinearMovementElement(FtPlantSimulation simulation, int length) {
+	public MovingElement(FtPlantSimulation simulation, int movementDistance) {
 
 		this.simulation = simulation;
-		this.length = length;
+		this.distance = movementDistance;
 		
 		int updateInterval = simulation.getUpdateInterval();
 		
 		// Calculate the duration it takes to move the workpiece along the whole conveyor (in ms)
-		int transportDuration = (length / actuatorVelocity) * 1000;
+		int transportDuration = (movementDistance / actuatorVelocity) * 1000;
 		
 		// Step size depends on the duration and the updateInterval. 
-		this.stepSize = length / ( transportDuration / updateInterval);
+		this.stepSize = movementDistance / ( transportDuration / updateInterval);
 	}
 
 }
