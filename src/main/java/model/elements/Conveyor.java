999a97cfbec22aabe8df0680e73337d7c663e4b0
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2020-05-25 15:11:58 +0200

    Removed leftovers of gui element

--- src/main/java/model/elements/Conveyor.java
+++ src/main/java/model/elements/Conveyor.java
@@ -1,142 +1,116 @@
 package model.elements;
 
 import gui.element.shape.ConveyorShape;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import model.simulation.FtPlantSimulation;
 
 /**
  * Special type of a {@link BinaryActuator} that represents a conveyor with a given length and methods to update workpiece position
  */
-public class Conveyor extends LinearMovementElement {
+public class Conveyor extends MovingElement {
 
 	private ConveyorShape shape;
-	
+
 	private BinaryActuator motorLeft, motorRight;
 
-	private BooleanProperty workpiecePresent = new SimpleBooleanProperty(false);
-	private SimpleIntegerProperty workpiecePosition = new SimpleIntegerProperty(0);
-	
-	private BooleanProperty motorLeftRunning = new SimpleBooleanProperty(false);
-	private BooleanProperty motorRightRunning = new SimpleBooleanProperty(false);
-	
-	public Conveyor(SimulationElementName elementName, ConveyorShape shape, ActuatorDefinition motorLeft, ActuatorDefinition motorRight, FtPlantSimulation simulation, int length) {
+	private boolean workpiecePresent = false;
+	private boolean workpieceBlocked = false;
+	private int workpiecePosition = 0;
+
+	public Conveyor(SimulationElementName elementName, ConveyorShape shape, ActuatorDefinition motorLeft, ActuatorDefinition motorRight,
+			FtPlantSimulation simulation, int length) {
 		super(simulation, length);
 		this.shape = shape;
+		this.shape.setLength(length);
 		this.simulationElementName = elementName;
 		this.motorLeft = new BinaryActuator(motorLeft, simulation);
 		this.motorRight = new BinaryActuator(motorRight, simulation);
-		
-		workpiecePresent.addListener(new ChangeListener<Boolean>() {
-			@Override
-			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-				if(newValue == false) {
-					shape.resetPosition();
-				}
-			}
-		});
-		
-		workpiecePosition.addListener(new ChangeListener<Number>() {
-			@Override
-			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-				if(workpieceIsPresent()) {
-					shape.setPosition(workpiecePosition.get());
-				}
-			}
-		});
-		
-		motorLeftRunning.addListener(new ChangeListener<Boolean>() {
-			@Override
-			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-				if(newValue == true) {
-					shape.activateLeft();
-				}else {
-					shape.deactivateLeft();
-				}
-			}
-		});
-		
-		motorRightRunning.addListener(new ChangeListener<Boolean>() {
-			@Override
-			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-				if(newValue == true) {
-					shape.activateRight();
-				}else {
-					shape.deactivateRight();
-				}
-			}
-		});
+
 	}
 
 	/**
 	 * Checks whether or not a workpiece is currently on this conveyor
+	 * 
 	 * @return True, if there's a workpiece on this conveyor
 	 */
 	public boolean workpieceIsPresent() {
-		return this.workpiecePresent.getValue();
+		return this.workpiecePresent;
 	}
 
 	/**
 	 * Moves a workpiece to this conveyor
 	 */
 	public void addWorkpiece() {
-		this.workpiecePresent.set(true);
+		this.workpiecePresent = true;
 	}
 
 	/**
 	 * Removes a workpiece from this conveyor
 	 */
 	public void removeWorkpiece() {
-		this.workpiecePresent.set(false);
-		this.workpiecePosition.set(0);
+		this.workpiecePresent = false;
+		this.shape.resetPosition();
+		this.workpiecePosition = 0;
 	}
 
-	
+	public void blockWorkpiece() {
+		this.workpieceBlocked = true;
+	}
+
+	public void unblockWorkpiece() {
+		this.workpieceBlocked = false;
+	}
 
 	/**
 	 * Calculate the relative workpiece position
+	 * 
 	 * @return Relative workpiece position (in a range from 0 to 100)
 	 */
 	public float getRelativeWorkpiecePosition() {
-		if (this.workpiecePresent.getValue()) {
-			return this.workpiecePosition.floatValue() / (float)this.length * 100;
+		if (this.workpiecePresent) {
+			return (float) this.workpiecePosition / (float) this.distance * 100;
 		} else {
 			return 0;
 		}
 	}
 
-	
 	public BinaryActuator getMotorLeft() {
 		return this.motorLeft;
 	}
-	
-	
+
 	public BinaryActuator getMotorRight() {
 		return this.motorRight;
 	}
-	
-	
+
 	/**
 	 * Updates method that is called in every simulation loop. Updates the current position of the workpiece if there is one on this conveyor.
 	 */
 	public void update() {
-		if (this.workpiecePresent.getValue() && this.motorLeft.isOn()) {
-			int newValue = Math.min(this.length, this.workpiecePosition.add(this.stepSize).intValue() );
-			this.workpiecePosition.set(newValue);
+		if (this.motorLeft.isOn() && this.workpiecePresent && !this.workpieceBlocked) {
+			this.workpiecePosition = Math.min(this.distance, this.workpiecePosition + this.stepSize);
+
+			this.shape.setRelativePosition(this.getRelativeWorkpiecePosition());
 		}
-		
-		motorLeftRunning.set(motorLeft.isOn());
-		motorRightRunning.set(motorRight.isOn());	
+
+		if (this.motorLeft.isOn()) {
+			shape.activateLeft();
+		} else {
+			shape.deactivateLeft();
+		}
+
+		if (this.motorRight.isOn()) {
+			shape.activateRight();
+		} else {
+			shape.deactivateRight();
+		}
 	}
-	
+
 	@Override
 	public void reset() {
-		shape.resetPosition();
-		shape.deactivateLeft();
-		shape.deactivateRight();
+		this.unblockWorkpiece();
+		this.removeWorkpiece();
+		this.shape.resetPosition();
+		this.shape.deactivateLeft();
+		this.shape.deactivateRight();
 	}
-	
+
 }
